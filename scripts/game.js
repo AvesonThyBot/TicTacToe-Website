a67fe6f6d8a159655a48e3875bdb9734e26b3e34
Aveson <avesoncarvalho25@gmail.com>
2023-09-26 20:33:53 +0100

    Finished website

--- scripts/game.js
+++ scripts/game.js
@@ -39,4 +39,78 @@ let winningPattern = [
     popupRef.classList.add("hide");
   };
 
+  //Win victory
+  const winFunction = (letter) => {
+    disableButtons();
+    if (letter == "X") {
+      msgRef.innerHTML = "&#x1F389; <br> 'X' Wins";
+    } else {
+      msgRef.innerHTML = "&#x1F389; <br> 'O' Wins";
+    }
+  };
+
+  //Function for draw
+  const drawFunction = () => {
+    disableButtons();
+    msgRef.innerHTML = "&#x1F60E; <br> It's a Draw";
+  };
+
+  //New Game
+  newgameBtn.addEventListener("click", () => {
+    count = 0;
+    enableButtons();
+  });
+  restartBtn.addEventListener("click", () => {
+    count = 0;
+    enableButtons();
+  });
+
+  //Win Logic
+  const winChecker = () => {
+    //Loop through all win patterns
+    for (let i of winningPattern) {
+      let [element1, element2, element3] = [
+        btnRef[i[0]].innerText,
+        btnRef[i[1]].innerText,
+        btnRef[i[2]].innerText,
+      ];
+      //Check if elements are filled
+      //If 3 empty elements are same and would give win as would
+      if (element1 != "" && (element2 != "") & (element3 != "")) {
+        if (element1 == element2 && element2 == element3) {
+          //If all 3 buttons have same values then pass the value to winFunction
+          winFunction(element1);
+        }
+      }
+    }
+  };
+
+  //Display X/O on click
+  btnRef.forEach((element) => {
+    element.addEventListener("click", () => {
+      if (xTurn) {
+        xTurn = false;
+        playerTurn.innerText = "O's Turn.";
+        //Display X
+        element.innerText = "X";
+        element.disabled = true;
+      } else {
+        xTurn = true;
+        playerTurn.innerText = "X's Turn.";
+        //Display O
+        element.innerText = "O";
+        element.disabled = true;
+      }
+      //Increment count on each click
+      count += 1;
+      if (count == 9) {
+        drawFunction();
+      }
+      //Check for win on every click
+      winChecker();
+    });
+  });
+
+  //Enable Buttons and disable popup on page load
+  window.onload = enableButtons;
   
