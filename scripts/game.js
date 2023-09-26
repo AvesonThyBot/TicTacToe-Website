9f72cad067a4f21055bf013492151d3b91ac5618
Aveson <avesoncarvalho25@gmail.com>
2023-09-26 19:55:09 +0100

    added all dom variable

---
+++ scripts/game.js
@@ -0,0 +1,6 @@
+// DOM Variables
+const btnRef = document.querySelectorAll(".button-option");
+const popupRef = document.querySelector(".popup");
+const newgameBtn = document.getElementById("new-game");
+const restartBtn = document.getElementById("restart");
+const msgRef = document.getElementById("message");
